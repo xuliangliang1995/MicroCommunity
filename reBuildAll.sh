f4a42c56bfe1ab0b913f32308a10633eaa7f3f7a
xuliang <835547206@qq.com>
2020-03-11 17:30:25 +0800

    c

--- reBuildAll.sh
+++ reBuildAll.sh
@@ -13,6 +13,6 @@ docker-compose -f ./StoreService/docker/docker-compose.yml       up -d --build -
 docker-compose -f ./UserService/docker/docker-compose.yml       up -d --build --force-recreate;
 docker-compose -f ./WebService/docker/docker-compose.yml       up -d --build --force-recreate;
 docker-compose -f ./FeeService/docker/docker-compose.yml       up -d --build --force-recreate;
-#docker-compose -f ./CommonService/docker/docker-compose.yml       up -d --build --force-recreate;
+docker-compose -f ./CommonService/docker/docker-compose.yml       up -d --build --force-recreate;
 # 删除无用镜像
 docker images|grep none|awk '{print $3 }'|xargs docker rmi
