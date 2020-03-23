0935eb125742c1f4a697dff43098ad7cb31e39a8
xuliangliang <835547206@qq.com>
2020-03-23 21:24:11 +0800

    添加商城服务

---
+++ MallService/src/main/java/com/java110/mall/MallServiceApplicationStart.java
@@ -0,0 +1,13 @@
+package com.java110.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallServiceApplicationStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallServiceApplicationStart.class, args);
+    }
+
+}
---
+++ MallService/src/test/java/com/java110/mall/MallServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java110.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
