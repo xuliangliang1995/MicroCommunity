ac9b66c0613d834999bdb6ae9a7e60a15127b441
xuliangliang <835547206@qq.com>
2020-03-23 17:06:52 +0800

    c

--- java110-core/src/main/java/com/java110/core/smo/owner/IOwnerDeliveryAddressInnerServiceSMO.java
+++ java110-core/src/main/java/com/java110/core/smo/owner/IOwnerDeliveryAddressInnerServiceSMO.java
@@ -3,10 +3,7 @@ package com.java110.core.smo.owner;
 import com.java110.core.feign.FeignConfiguration;
 import com.java110.dto.owner.OwnerDeliveryAddressDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,6 +43,6 @@ public interface IOwnerDeliveryAddressInnerServiceSMO {
      * @param memberId
      * @return
      */
-    @RequestMapping(value = "/{memberId}/ownerDeliveryAddress")
+    @GetMapping(value = "/{memberId}/ownerDeliveryAddress")
     OwnerDeliveryAddressDto getOwnerDeliveryAddress(@PathVariable("memberId") String memberId);
 }
